3fad080718229f607e0ed8e51d41a5f2618d7606
Todor Ivanov <tivanov@tinqin.com>
2015-09-28 11:22:30 +0300

    updates

--- deployment/rebuild.sh
+++ deployment/rebuild.sh
@@ -2,4 +2,4 @@
 cp -avr ../bills ./bills
 (docker ps -a | grep 'apache-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
 (docker build -t apache-bills .)
-docker run --name apacheBills --restart='always' -d -p 92:80 apache-bills
+docker run --name apacheBills --restart='always' -d -p 80:80 apache-bills
