9a9c55f035c70be56ce1fcada17c72c1628b83d2
Todor Ivanov <tivanov@tinqin.com>
2015-09-28 10:44:41 +0300

    init laravel project

---
+++ deployment/rebuild.sh
@@ -0,0 +1,5 @@
+(rm -rf webapp)
+cp -avr ../bills ./bills
+(docker ps -a | grep 'apache-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
+(docker build -t apache-bills .)
+docker run --name apacheBills --restart='always' -d -p 92:80 apache-bills
