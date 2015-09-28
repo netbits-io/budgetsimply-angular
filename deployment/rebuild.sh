91b8a6cc79edc366193493b5c3f1c94dec8ab428
Todor Ivanov <tivanov@tinqin.com>
2015-09-28 11:00:44 +0300

    updates

--- deployment/rebuild.sh
+++ deployment/rebuild.sh
@@ -1,4 +1,4 @@
-(rm -rf webapp)
+(rm -rf bills)
 cp -avr ../bills ./bills
 (docker ps -a | grep 'apache-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
 (docker build -t apache-bills .)
