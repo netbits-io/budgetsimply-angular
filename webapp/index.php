7d9b0284e9d62229e04cf2e876a1840125d0f94a
todor.ait@gmail.com <todor.ait@gmail.com>
2015-10-03 14:36:47 +0300

    Merge origin/master
    Conflicts:
    	deployment/apache-config.conf

--- webapp/index.php
+++ webapp/index.php
@@ -1,9 +1,19 @@
-testttta
-<?php
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>simplybudget.io</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This is some text.</p>
+</div>
 
+</body>
+</html>
