fbf653f10b1459d6fd1c1623995716f11559b2c7
todor.ait@gmail.com <todor.ait@gmail.com>
2015-10-03 11:12:01 +0300

    remove the horrible laravel framework

--- deployment/rebuild.sh
+++ deployment/rebuild.sh
@@ -1,5 +1,5 @@
-(rm -rf ./bills)
-cp -avr ../bills ./bills
+(rm -rf ./webapp)
+cp -avr ../webapp ./webapp
 (docker ps -a | grep 'apache-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
 (docker build -t apache-bills .)
 docker run --name apacheBills --restart='always' -d -p 90:80 apache-bills
