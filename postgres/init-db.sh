f0cffd8b3327a83c4b052f8bf7c529d196068eb7
todor.ait@gmail.com <todor.ait@gmail.com>
2015-10-04 15:36:14 +0300

    experimenting..

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -10,7 +10,7 @@ gosu postgres postgres --single -jE bills <<- EOSQL
     id bigint NOT NULL DEFAULT nextval('images_seq'),
     email_id varchar(255) DEFAULT NULL,
     first_name varchar(255) DEFAULT NULL,
-    last_name varchar(255) DEFAULT NULL
+    last_name varchar(255) DEFAULT NULL,
     password varchar(255) DEFAULT NULL,
     contact_no varchar(100) DEFAULT NULL,
     updated_at timestamp DEFAULT NULL,
