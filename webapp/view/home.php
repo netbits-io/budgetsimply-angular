8a7e16ee89e342ba5cd2be8a8672a9d39c3c0cc9
todor.ait@gmail.com <todor.ait@gmail.com>
2015-10-15 00:18:29 +0300

    experimenting..

--- webapp/view/home.php
+++
@@ -1,8 +0,0 @@
-<?php include_once 'layout/header.php'; ?>
-
-<div class="container">
-    <h1>My First Bootstrap Page</h1>
-    <p>This is some text.</p>
-</div>
-
-<?php include_once 'layout/footer.php'; ?>
