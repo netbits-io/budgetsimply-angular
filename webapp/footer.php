188961eb55124e16e69630b9c86320ff09f96d0a
todor.ait@gmail.com <todor.ait@gmail.com>
2015-10-14 23:03:36 +0300

    experimenting..

--- webapp/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
- <div class="alert alert-info">
-    <strong>tutorial !</strong> <a href="http://cleartuts.blogspot.com/">cleartuts</a>!
- </div>
-</div>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
---
+++ webapp/view/home.php
@@ -0,0 +1,8 @@
+<?php include_once 'layout/header.php'; ?>
+
+<div class="container">
+    <h1>My First Bootstrap Page</h1>
+    <p>This is some text.</p>
+</div>
+
+<?php include_once 'layout/footer.php'; ?>
