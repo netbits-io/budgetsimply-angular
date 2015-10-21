d9f9d94f0a5a17d3466d39b356d1ad7b2c0b8b1d
Todor Ivanov <tivanov@tinqin.com>
2015-10-21 12:05:17 +0300

    experimenting...

--- deployment/rebuild.sh
+++
@@ -1,5 +0,0 @@
-(rm -rf ./webapp)
-cp -r ../webapp ./webapp
-(docker ps -a | grep 'apache-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
-(docker build -t apache-bills .)
-docker run --name apacheBills --restart='always' -d -p 90:80 apache-bills
