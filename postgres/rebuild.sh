1e32418c9d97787e7db566495bb5bd58337ffd6e
Todor Ivanov <tivanov@tinqin.com>
2015-10-21 12:01:43 +0300

    experimenting...

--- postgres/rebuild.sh
+++
@@ -1,3 +0,0 @@
-(docker ps -a | grep 'postgres-bills' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f)
-(docker build --rm=false -t postgres-bills .)
-docker run --name postgresBills --restart='always' -d -p 5422:5432 postgres-bills
